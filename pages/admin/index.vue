31372ed9ac696e21940ef6c274a20e93c7e17a14
leo03164 <leo03164@gmail.com>
2022-04-03 20:44:51 +0800

    Change button style
    修改 button style to AppButton

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,13 +1,15 @@
 <template>
-    <div class="admin-page">
-        <section class="new-post">
-            <button @click="$router.push('/admin/new-post')">Create Post</button>
-        </section>
-        <section class="existing-posts">
-            <h1>Existing Posts</h1>
+  <div class="admin-page">
+    <section class="new-post">
+      <AppButton @click="$router.push('/admin/new-post')"
+        >Create Post</AppButton
+      >
+    </section>
+    <section class="existing-posts">
+      <h1>Existing Posts</h1>
       <post-list isAdmin />
-        </section>
-    </div>
+    </section>
+  </div>
 </template>
 
 <script>
@@ -25,16 +27,16 @@ export default {
 
 <style scoped>
 .admin-page {
-    padding: 20px;
+  padding: 20px;
 }
 
 .new-post {
-    text-align: center;
-    border-bottom: 2px solid #ccc;
-    padding-bottom: 10px;
+  text-align: center;
+  border-bottom: 2px solid #ccc;
+  padding-bottom: 10px;
 }
 
 .existing-post h1 {
-    text-align: center;
+  text-align: center;
 }
 </style>
