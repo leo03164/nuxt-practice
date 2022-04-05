bcdd55990c7f811024a3ddfd3ae2bbdabda93e77
leo03164 <leo03164@gmail.com>
2022-04-05 15:30:42 +0800

    add: dynamic postlist

--- components/Posts/PostList.vue
+++ components/Posts/PostList.vue
@@ -1,48 +1,38 @@
 <template>
-    <section class="posts-list">
-      <PostPreview
-        id="1"
-        :is-admin="isAdmin"
-        thumbnail="http://img.alicdn.com/imgextra/i1/1122991732/O1CN01qw4HDf1OfIYVUPkr1_!!1122991732.jpg"
-        title="First"
-        previewText="First One"
-      />
-      <PostPreview
-        id="2"
-        :is-admin="isAdmin"
-        thumbnail="http://img.alicdn.com/imgextra/i1/1122991732/O1CN01qw4HDf1OfIYVUPkr1_!!1122991732.jpg"
-        title="Second"
-        previewText="Second One"
-      />
-      <PostPreview
-        id="3"
-        :is-admin="isAdmin"
-        thumbnail="http://img.alicdn.com/imgextra/i1/1122991732/O1CN01qw4HDf1OfIYVUPkr1_!!1122991732.jpg"
-        title="Third"
-        previewText="Third One"
-      />
-      
-    </section>
+  <section class="posts-list">
+    <PostPreview
+      v-for="post of posts"
+      :key="post.id"
+      :id="post.id"
+      :is-admin="isAdmin"
+      :thumbnail="post.thumbnail"
+      :title="post.title"
+      :previewText="post.previewText"
+    />
+  </section>
 </template>
 
 <script>
 import PostPreview from "@/components/Posts/PostPreview";
 
-export default {  
-  components:{
-    PostPreview
+export default {
+  components: {
+    PostPreview,
   },
-  props:{
-    isAdmin:{
+  props: {
+    isAdmin: {
       type: Boolean,
-      default: false
-    }
-  }
-}
+      default: false,
+    },
+    posts: {
+      type: Array,
+      require: true,
+    },
+  },
+};
 </script>
 
 <style scoped>
-
 .posts-list {
   display: flex;
   padding: 20px;
--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,24 +1,43 @@
 <template>
-    <div class="posts-page">
-      <PostList/>
-    </div>
+  <div class="posts-page">
+    <PostList :posts="loadedPost" />
+  </div>
 </template>
 
 <script>
 import PostList from "@/components/Posts/PostList";
 
-export default {  
-  components:{
-    PostList
-  }
-}
+export default {
+  components: {
+    PostList,
+  },
+  data() {
+    return {
+      loadedPost: [
+        {
+          id: "leo",
+          title: "about nancy",
+          previewText: "she is my girl friend",
+          thumbnail:
+            "https://thepolysh.com/blog/wp-content/uploads/2018/11/WhooliChen_cover.png",
+        },
+        {
+          id: "nancy",
+          title: "about leo",
+          previewText: "he is my boy friend",
+          thumbnail:
+            "https://thepolysh.com/blog/wp-content/uploads/2018/11/WhooliChen_cover.png",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style scoped>
-.posts-page{
+.posts-page {
   display: flex;
   justify-content: center;
   align-items: center;
 }
-
 </style>
