5d7f00489f45035f2b473af058942e926650da93
leo03164 <leo03164@gmail.com>
2022-04-05 21:31:10 +0800

    eslint

--- components/Admin/AdminPostForm.vue
+++ components/Admin/AdminPostForm.vue
@@ -1,22 +1,26 @@
 <template>
   <form @submit.prevent="onSave">
-        <AppControlInput v-model="editedPost.author">Author Name</AppControlInput>
-        
-        <AppControlInput v-model="editedPost.title">Title</AppControlInput>
-                
-        <AppControlInput v-model="editedPost.thumbnailLink">Thumbnail Link</AppControlInput>
-                
-        <AppControlInput
-            control-type="textarea"
-            v-model="editedPost.content">Content</AppControlInput>
-                
-        <AppButton type="submit">Save</AppButton>
-                
-        <AppButton
-            type="button"
-            style="margin-left: 10px"
-            btn-style="cancel"
-            @click="onCancel">Cancel</AppButton>
+    <AppControlInput v-model="editedPost.author">Author Name</AppControlInput>
+
+    <AppControlInput v-model="editedPost.title">Title</AppControlInput>
+
+    <AppControlInput v-model="editedPost.thumbnailLink"
+      >Thumbnail Link</AppControlInput
+    >
+
+    <AppControlInput control-type="textarea" v-model="editedPost.content"
+      >Content</AppControlInput
+    >
+
+    <AppButton type="submit">Save</AppButton>
+
+    <AppButton
+      type="button"
+      style="margin-left: 10px"
+      btn-style="cancel"
+      @click="onCancel"
+      >Cancel</AppButton
+    >
   </form>
 </template>
 
@@ -25,39 +29,37 @@ import AppControlInput from "@/components/UI/AppControlInput";
 import AppButton from "@/components/UI/AppButton";
 
 export default {
-    components:{
-        AppControlInput,
-        AppButton
+  components: {
+    AppControlInput,
+    AppButton,
+  },
+  props: {
+    post: {
+      type: Object,
+      require: false,
     },
-    props:{
-        post:{
-            type: Object,
-            require: false
-        }
+  },
+  data() {
+    return {
+      editedPost: this.post
+        ? { ...this.post }
+        : {
+            author: "",
+            title: "",
+            thumbnailLink: "",
+            content: "",
+          },
+    };
+  },
+  methods: {
+    onSave() {
+      this.$emit("submit", this.editedPost);
     },
-    data(){
-        return {
-            editedPost: this.post 
-            ? {...this.post} 
-            : {
-                author: '',
-                title: '',
-                thumbnailLink: '',
-                content: ''
-            }
-        }
+    onCancel() {
+      this.$router.push("/admin");
     },
-    methods: {
-        onSave(){
-            console.log(this.editedPost);
-        },
-        onCancel(){
-            this.$router.push('/admin')
-        }
-    }
-}
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
